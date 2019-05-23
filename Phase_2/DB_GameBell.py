1148b079cfa6ebe25a86aaf1bfe8b40371570f62
Darlight <andres.perdomo09@gmail.com>
2019-05-23 16:17:36 -0600

    Updated more functions to DB. Need someone to test it.

--- Phase_2/DB_GameBell.py
+++ Phase_2/DB_GameBell.py
@@ -3,7 +3,7 @@ Universidad del Valle de Guatemala
 Seccion
 DB_GameBell
 """
-from neo4j import GraphDatabase
+from neo4j import GraphDatabase, basic_auth
 from neo4jrestclient import client
 
 
@@ -24,19 +24,31 @@ class DB_GameBell(object):
         # Just closes the driver
         self._driver.close()
 
+    def checkUser(self, user, password):
+        try:  # If user doesn´t exist it will show an excpetion
+            passwordToCheck = self.__driver.getNode("Person", "name", self.encriptPassword(user)).single()[0]["password"]
+            if (self.unencriptPassword(passwordToCheck) == password):
+                self.user = self.encriptPassword(user)
+        except:
+            return False
+        return (self.unencriptPassword(passwordToCheck) == password)
     def add_Person(self, name, age, email, password): #nombre, edad, email, y contrasela
         # Creating a Patient node with its atributes
         self.person.add(self._driver.nodes.create(name=name, age= age , email = email, password = password))
         return "Done creating a new username in the database. \n"
-    def delete_Person(self, name):
-        result = "MATCH " #Need to learn
+
+    #key: it receives a key or a reference that the node has
+    #value: it recives de value of the reference key.
+    def delete_Person(self,key,value):
+        result = "MATCH (a:Person)\nWHERE a." + key + "= $value\nDETACH DELETE (a)"
+        with self._driver.session() as session:
+            session.write_transaction(self._delete,result,value)
 
     #Creates a connection with another data user.
     def add_PersonConnection(self,name1,name2):
         name1.relationships.create("KNOWS",name2)
         return "Database has been updated"
     def change_Password(self,new_password,name):
-        
         q = "MATCH (p:Person WHERE p.name = \"{0}\" AND p.password = \"{3}\" + RETURN p".format(name,new_password) + "SET p.password = "
     #Grabs all the games and shows to the user
     def get_AllGame(self):
@@ -47,126 +59,35 @@ class DB_GameBell(object):
             games.append(node[0]["name"])
         return games
 
-    def game_Recommendation(self):
+    #Grabs all the companies to show
+    def get_AllCompanies(self):
+        result = "MATCH (a:Company)\nRETURN a"
+        with self._driver.session() as session:
+            return session.write_transaction(self._Default, result)
+
+def game_Recommendation(self):
         return "hello"
 
+    @staticmethod
+    def _Default(tx, result):
+        return tx.run(result)
 
-"""
-    def add_PatientDoctorConnetion(self, patientName, doctorName):
-        q = "MATCH (p:Patient), (d:Doctor) WHERE p.name = \"{0}\" AND d.name = \"{0}\" RETURN p,d".format(patientName,
-                                                                                                          doctorName)
-        results = self._driver.query(q, returns=(client.Node, client.Node))
-        for i in results:
-            patient = i[0]
-            doctor = i[1]
-            patient.relationships.create("Visits", doctor)
-        return True
+    @staticmethod
+    def _getNodes(tx, result, value):
+        return tx.run(result, value=value)
 
-    def add_PatientDrugConnection(self, patientName, drugName):
-        q = "MATCH (p:Patient), (m:Drug) WHERE p.name = \"{0}\" AND m.name = \"{0}\" RETURN p,m".format(patientName,
-                                                                                                        drugName)
-        results = self._driver.query(q, returns=(client.Node, client.Node))
-        for i in results:
-            patient = i[0]
-            drug = i[1]
-            patient.relationships.create("Takes", drug)
-        return True
+    @staticmethod
+    def _getNode(tx, result, value):
+        return tx.run(result, value=value)
 
-    def add_DoctorDrugConnection(self, doctorName, drugName):
-        q = "MATCH (d:Doctor), (m:Drug) WHERE d.name = \"{0}\" AND m.name = \"{0}\" RETURN p,m".format(doctorName,
-                                                                                                       drugName)
-        results = self._driver.query(q, returns=(client.Node, client.Node))
-        for i in results:
-            doctor = i[0]
-            drug = i[1]
-            doctor.relationships.create("Prescribes", drug)
-        return True
+    @staticmethod
+    def _upgrade(tx, result, value, newValue):
+        result = tx.run(result, value=value, newValue=newValue)
 
-    def add_PatientPatientConnection(self, patient1, patient2):
-        q = "MATCH (p:Patient), (a:Patient) WHERE p.name = \"{0}\" AND a.name = \"{0}\" RETURN p,a".format(patient1,
-                                                                                                           patient2)
-        results = self._driver.query(q, returns=(client.Node, client.Node))
-        for i in results:
-            patient1s = i[0]
-            patient2s = i[1]
-            patient1s.relationships.create("Knows", patient2s)
-        return True
+    @staticmethod
+    def _deleteLink(tx, result, variable1, variable2):
+        result = tx.run(result, variable1=variable1, variable2=variable2)
 
-    def add_DoctorPatientConnection(self, doctorName, patientName):
-        q = "MATCH (d:Doctor), (p:Patient) WHERE d.name = \"{0}\" AND p.name = \"{0}\" RETURN d,p".format(doctorName,
-                                                                                                          patientName)
-        results = self._driver.query(q, returns=(client.Node, client.Node))
-        for i in results:
-            doctor = i[0]
-            patient = i[1]
-            doctor.relationships.create("Knows", patient)
-        return True
-
-    # Creates a list of doctors with the given specialty
-    def find_DoctorsWithSpec(self, spec):
-        doctors = []
-        q = "MATCH (d:Doctor) WHERE d.specialty = \"{0}\" RETURN d".format(spec)
-        results = self._driver.query(q, returns=client.Node)
-        for node in results:
-            doctors.append(node[0]["name"])  # adds doctor name
-        return doctors
-
-    # Crates a session with a doctor created beforehand and prescribes the drug
-
-    def apoint_Session(self, patientName, patientTel, doctorName, drugName, date1, date2, dose):
-        self.add_Patient(patientName, patientTel)
-        self.add_Drug(drugName, date1, date2, dose)
-        q1 = "MATCH (d:Doctor) WHERE d.specialty = \"{0}\" RETURN d".format(doctorName)
-        q2 = "MATCH (p:Patient) WHERE d.specialty = \"{0}\" RETURN p".format(patientName)
-        q3 = "MATCH (m:Drug) WHERE d.specialty = \"{0}\" RETURN m".format(drugName)
-        q2.relationships.create("VISITS", q1)
-        q1.relationships.create("PRESCRIBES", q3)
-        q2.relationships.create("TAKES", q3)
-
-    def get_PatientKnownPeople(self, patientName):
-        knownPatients = []
-        q = "MATCH (p:Pattient)-[k:KNOWS"
-
-    def recommend_Doctor(self, patientName, spec):
-        doctorsSpec = []  # Lista de doctores con especialidad especifica
-        doctorsKnown = []  # Lista de doctores conocidos por amigos
-        doctorsRecommended = []  # Lista de doctores recomendados
-
-        friends = []  # Lista de amigos del paciente
-
-        # Se obtiene la lista de conocidos del paciente
-        q1 = "MATCH (p:Patient)-[r:Knows]->(f:Patient) WHERE p.name = \"{0}\" RETURN p, type(r), f".format(patientName)
-        results = self._driver.query(q1, returns=(client, Node, str, client.Node))
-        for friend in results:
-            friends.append(node[2]["name"])
-
-        # Se obtiene la lista de doctores con una especialidad especifica
-        doctorsSpec = find_DoctorsWithSpec(spec)
-
-        # Si el paciente tiene amigos o conocidos
-        # Si la lista de amigos tiene tamano diferente de cero
-        if (friends != None):
-            # Para cada paciente en la lista de amigos se obtiene el nombre del amigo
-            for patientName in friends:
-                q2 = "MATCH (p:Patient)-[r:Knows]->(f.Patient) WHERE p.name = \"{0}\" RETURN f".format(patientName)
-                results = self._driver.query(query, returns=client.Node)
-
-                for f in results:
-                    friends.append(f[0]["name"])
-
-            # Doctores por especialidad
-            # Para cada doctor con especialidad
-            for x in range(len(doctorsSpec)):
-                # Para cada doctor conocido
-                # Se obtiene el nombre de los doctores conocidos y con la especialidad
-                for y in range(len(doctorsKnown)):
-                    q = "MATCH (p:Patient)-[r:Visits]->(d:Doctor) WHERE p.name = \"{0}\" AND d.name = \"{0}\" RETURN p, d".format(
-                        doctorsKnown[y], doctorsSpec[x])
-                    results = self._driver.query(q, returns=(client.Node, client.Node))
-                    for z in results:
-                        doctorsRecommended.append(node[1]["name"])
-
-            # Retorna la lista con los nombres de los doctores conocidos y con una especialidad especifica
-            return doctorsRecommended
-
-"""
+    @staticmethod
+    def _delete(tx, result, value):
+        result = tx.run(result, value=value)
