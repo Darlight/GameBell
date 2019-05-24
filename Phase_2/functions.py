bba8370688999e932f7f9054d311ea32d5b6aab0
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2019-05-24 10:14:24 -0600

    Revert "Adding comments"
    This reverts commit 1c93b5887664c299d348e91bc710c89bcb37d216.

--- Phase_2/functions.py
+++ Phase_2/functions.py
@@ -1,4 +1,4 @@
-def add_User(db: object, name: object, age: object, email: object, password: object) -> object:
+def add_User(db, name, age, email, password):
 	info = "CREATE (user:User {name:" + str(name) +", age:" + str(age) + "email:" + str(email) + ", password:" + str(password) + "});"
 	db.query(info)
 def add_Game(db, title, price, genre):
