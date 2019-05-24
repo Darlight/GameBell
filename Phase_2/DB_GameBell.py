b11a0ad3320847dd794185f6d4807de8eb0ba469
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2019-05-24 01:50:59 -0600

    FINISHED EVERYTHING
    Finally

--- Phase_2/DB_GameBell.py
+++ Phase_2/DB_GameBell.py
@@ -9,41 +9,94 @@ from neo4jrestclient import client
 
 def add_User(db, name, age, email, password):
     Person = db.nodes.create(name=name, age = age, email = email, password = password)
-    Person.labels.add("User")
-    print ("\nUser has been registered succesfully.\n")
+    try:
+    	Person.labels.add("User")
+    	print ("\nUser has been registered succesfully.\n")
+    except Exception:
+    	print("\nUser could not be added.\n")
 def add_Game(db, title, price, rating):
     Game = db.nodes.create(title=title,price= price, rating=rating)
-    Game.labels.add("Game")
-    print("\nGame has been registered succesfully.\n")
+    try:
+    	Game.labels.add("Game")
+    	print("\nGame has been registered succesfully.\n")
+    except Exception:
+    	print("\nGame could not be added.")
 def create_PersonConnection(user1,user2):
-    user1.relationships.create("KNOWS",user2)
-    return "Database has been updated. \n"
+    try:
+    	user1.relationships.create("KNOWS",user2)
+    	return "Database has been updated. \n"
+    except Exception:
+    	print("\n\n---------------------------------------------------------")
+    	print("-------------Couldn't add user to the Database------------")
+    	print("----------------------------------------------------------")
 def check_Prices(db, price, client):
     q = "Match(g: Game) WHERE g.price=" +price+" \n RETURN g"
     result = db.query(q,returns=(client.Node,float,client.Node))
-    for game in result:
-        print(game[0]["title"])
-    return game
-def check_Rating(db, rating, game):
+    try:
+    	for game in result:
+        	print(game[0]["title"])
+    	return game
+    except Exception:
+    	print("\n\n---------------------------------------------------------")
+    	print("----------Couldn't find games under such Price----------")
+    	print("----------------------------------------------------------")
+def get_User(db, username, client):
+	users = []
+	q = 'MATCH(p: Person) WHERE p.name="'+username+'" RETURN p'
+	result = db.query(q, returns=(client.Node, str, client.Node))
+	try:
+		for user in result:
+			if user.count(user)<1:
+				users.append(user)
+	except Exception:
+		print("----------------------------------------------------------")
+		print("-----------------Couldn't find this user------------------")
+		print("----------------------------------------------------------")
+	
+	if not users:
+	   	return False
+	elif users:
+		return True
+	
+	
+
+def check_Rating(db, rating, client):
     q = 'MATCH (g: Game) WHERE g.rating="' + rating + '" RETURN g'
     result = db.query(q, returns=(client.Node, str, client.Node))
-    for r in result:
-        print(r[0]['name'])
-    return game
+    try:
+    	for r in result:
+        	print(r[0]['title'])
+    	return r
+    except Exception:
+    	print("\n\n---------------------------------------------------------")
+    	print("----------Couldn't find games under such rating----------")
+    	print("----------------------------------------------------------")
+
 def recommendGame(db, price, rating):
     q = 'MATCH (g: Game) WHERE g.price="'+price+'" RETURN g'
     result = db.query(q, returns=(client.Node, str, client.Node))
     games = []
-    for game in result:
-    	if games.count(game)<1:
-    		games.append(game)
+    try:
+    	for game in result:
+    		if games.count(game)<1:
+    			games.append(game)
+    except Exception:
+    	print("---------------------------------------------------------")
+    	print("----------Couldn't find games under such price----------")
+    	print("----------------------------------------------------------")
 
     q = 'MATCH (g: Game) WHERE g.rating="'+rating+'" RETURN g'
     result = db.query(q, returns=(client.Node, str, client.Node))
-    for game in result:
-    	if games.count(game)<1:
-    		games.append(game)
-    ## Punteo
+    try:
+    	for game in result:
+    		if games.count(game)<1:
+    			games.append(game)
+    except Exception:
+    	print("---------------------------------------------------------")
+    	print("----------Couldn't find games under such rating----------")
+    	print("----------------------------------------------------------")
+    ## Finishing
+    
     for game in games:
     	if game[0]['price'] == price:
     		numero = int(game[0]['rating'])
--- Phase_2/MainFile.py
+++ Phase_2/MainFile.py
@@ -5,51 +5,26 @@ from DB_GameBell import *
 
 db = GraphDatabase("http://localhost:7474", username= "neo4j", password="12345")
 
-#Lets start by creating the users
-
-user = db.labels.create("User")
-code_1 = db.nodes.create(name="Mario", age="19", username="DarkSins", password="furries")
-user.add(code_1)
-code_2 = db.nodes.create(name="Josue", age="20", username="Saints", password="manuela")
-user.add(code_2)
-code_3 = db.nodes.create(name="Andres", age="20", username="LoboAnima", password="whytho")
-
-#Just some code so that the program doesn't crash on launch
-
-games = db.labels.create("Games")
-j1 = db.nodes.create(title="The Last of Us", price = "9.99", genre="Action", rating="5")
-j2 = db.nodes.create(title="Super Mario Bros", price= "0.99", genre="Arcade", rating="4")
-j3 = db.nodes.create(title="Pac man", price= "0.99", genre="Arcade", rating="4")
-j4 = db.nodes.create(title="Super Mario Sunshine", price= "4.99", genre="Platformer", rating="5")
-j5 = db.nodes.create(title="Halo", price="49.99", genre="Action", rating="5")
-j6 = db.nodes.create(title="Sonic", price="9.99", genre="Platformer", rating="3")
-j7 = db.nodes.create(title="Dark Souls", price="59.99", genre="F you", rating="5")
 
 
-games.add(j1, j2, j3, j4, j5, j6, j7)
-
-#Relationships using prices
-j1.relationships.create("Under10", j2)
-j1.relationships.create("Under10", j3)
-j1.relationships.create("Under10", j4)
-j1.relationships.create("Under10", j6)
-j2.relationships.create("Under10", j3)
-j2.relationships.create("Under10", j4)
-j2.relationships.create("Under10", j6)
-j3.relationships.create("Under10", j4)
-j3.relationships.create("Under10", j6)
-j6.relationships.create("Fullprice", j7)
-
-#Relationships using genre
-j1.relationships.create("Action", j5)
-j2.relationships.create("Arcade", j3)
-j4.relationships.create("Platformer", j6)
-
-
-status = True
+status_quo = True
+while status_quo:
+	option1 = input("Do you wish to register (1), or go on as guest? (2)\n>>> ")
+	if option1 == '1':
+		name = input("Please, add a username for the user: \n>>> ")
+		age = input("What's the age of the user? \n>>> ")
+		email = input("What's the user's email? \n>>> ")
+		password = input("What'll be the user's password? \n>>> ")
+		add_User(db, name, age, email, password)
+	if option1 == '2':
+		print("Guest session")
+		status = True
+		status_quo = False
+	else:
+		print("\nPlease try again.\n\n")
 
 while status:
-	print("Please choose an option from the following: \n\n1. Add a new user. \n2. Add a new game \n3. Find games by price \n4. Find games by Rating \n5. Recommend games \n6. Exit the program")
+	print("\n\nPlease choose an option from the following: \n\n1. Add a new user. \n2. Add a new game \n3. Find games by price \n4. Find games by Rating \n5. Recommend games \n6. Exit the program")
 	option = input(">>> ")
 
 	if option == '1':
@@ -68,12 +43,17 @@ while status:
 		check_Prices(db, price, client)
 	elif option == '4':
 		rating = input("What rating do you wish to have? (1 - 5) \n>>> ")
-		check_Rating(db, rating, client)
+		if rating == '1' or '2' or '3' or '4' or '5':
+			check_Rating(db, rating, client)
+		else:
+			print("Please, try again\n\n")
 	elif option == '5':
-		genre_given = input("What like of genre would you like to have recommended? \n>>> ")
-		price_given = input("What about the price?  \n>>> ")
-		rating = input("Rating? \n>>> ")
-		recommendGame(db, price_given, rating)
+		price_given = input("What about the price of the game?  \n>>> ")
+		rating = input("Rating (1 - 5)? \n>>> ")
+		if rating == '1' or '2' or '3' or '4' or '5':
+			recommendGame(db, price_given, rating)
+		else:
+			print("Invalid number. Please try again.\n\n")
 	elif option == '6':
 		status = False
 	else:
