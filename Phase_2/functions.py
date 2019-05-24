fd1a6156bab105fdd3ab61592c0235a81e2e5319
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2019-05-23 23:06:28 -0600

    Finished functions

--- Phase_2/functions.py
+++ Phase_2/functions.py
@@ -1,1 +1,6 @@
-def add_User(db, name, )
+def add_User(db, name, age, email, password):
+	info = "CREATE (user:User {name:" + str(name) +", age:" + str(age) + "email:" + str(email) + ", password:" + str(password) + "});"
+	db.query(info)
+def add_Game(db, title, price, genre):
+	info = "CREATE (game:Game {title:" +title+", price:"+price+", genre:"+genre+"});"
+    db.query(info)
